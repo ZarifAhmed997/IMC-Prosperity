e230cf40db2f593512d65c890fb67e1079ba97ec
Zarif Ahmed <zarifinzarif@gmail.com>
2026-04-16 22:37:48 +0100

    Added template of the algorithm

---
+++ ROUND1/algorithms/Trader.py
@@ -0,0 +1,51 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List
+import string
+
+class Trader:
+    
+    def run(self, state: TradingState):
+
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
+
+        # Orders to be placed on exchange matching engine
+        result = {}
+        for product in state.order_depths:
+            order_depth: OrderDepth = state.order_depths[product]
+            orders: List[Order] = []
+
+            acceptable_price = 0
+
+            # Calculate acceptable price based on current position and observations 
+
+            if product == "intarian_pepper_root":
+                acceptable_price = 
+
+            # MY SECTION 
+
+            elif product == "corn":
+                acceptable_price = 500
+
+            print("Acceptable price : " + str(acceptable_price))
+            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+
+            # Check if it surpasses the acceptable price threshold, if so buy/sell
+
+            if len(order_depth.sell_orders) != 0:
+                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                if int(best_ask) < acceptable_price:
+                    print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -best_ask_amount))
+    
+            if len(order_depth.buy_orders) != 0:
+                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                if int(best_bid) > acceptable_price:
+                    print("SELL", str(best_bid_amount) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -best_bid_amount))
+            
+            result[product] = orders
+    
+        traderData = ""  # No state needed - we check position directly
+        conversions = 0
+        return result, conversions, traderData
