2f384619d8798e641f4daf9148381c2d731e42f8
Zarif Ahmed <zarifinzarif@gmail.com>
2026-04-16 18:11:08 +0100

    initial

---
+++ deviation.py
@@ -0,0 +1,36 @@
+#Plot the deviation from the last price to the next price for a given symbol. (Seems to be fixed for data)
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+FILE_PATH = 'TUTORIAL_ROUND_1/trades_round_0_day_-1.csv'
+
+SYMBOL = 'TOMATOES'
+
+file = pd.read_csv(FILE_PATH, sep=';') 
+data = file[file['symbol'] == SYMBOL].copy()
+
+data['price'] = pd.to_numeric(abs(data['price'] - data['price'].shift(1)))
+
+clean_data = data.groupby('timestamp')['price'].mean().reset_index()
+
+plt.figure(figsize=(12, 6))
+plt.plot(clean_data['timestamp'], clean_data['price'], label='Mean Execution Price', color='#2ca02c')
+
+plt.scatter(data['timestamp'], data['price'], color='black', s=10, alpha=0.3, label='Individual Trades')
+
+plt.title(f'{SYMBOL} Deviation from last price')
+plt.xlabel('Timestamp (ms)')
+plt.ticklabel_format(style='plain', axis='x')
+plt.ylabel('Price')
+plt.grid(True, linestyle=':', alpha=0.7)
+plt.legend()
+
+min_price = data['price'].min()
+max_price = data['price'].max()
+
+plt.ylim(min_price - max_price) 
+plt.xlim(0, clean_data['timestamp'].max() + 2000)
+
+plt.tight_layout()
+plt.show()
