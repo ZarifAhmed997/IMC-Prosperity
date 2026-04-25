c2b89d1fc6d4be6acc5d2e02b4d659ab614ee4db
Zarif Ahmed <zarifinzarif@gmail.com>
2026-04-26 00:13:17 +0100

    finish this

--- ROUND_3/plot_trades.py
+++ ROUND_3/plot_trades.py
@@ -1,20 +1,46 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-PRODUCT = 'VEV_4000' # Change this to the product you want to plot (VELVETFRUIT_EXTRACT, HYDROGEN_PACK, or VEV_4000)
-FILE = 'ROUND_3/trades_round_3_day_0.csv'
+PRODUCT1 = 'VEV_5400'
+PRODUCT2 = 'VELVETFRUIT_EXTRACT'
+
+FILE1 = 'ROUND_3/trades_round_3_day_0.csv'
+FILE2 = 'ROUND_3/trades_round_3_day_1.csv'
+FILE3 = 'ROUND_3/trades_round_3_day_2.csv'
 
 # Load the data
-data = pd.read_csv(FILE, sep=';')
-data = data[data['symbol'] == PRODUCT]
+data1 = pd.read_csv(FILE1, sep=';')
+data2 = pd.read_csv(FILE2, sep=';')
+data3 = pd.read_csv(FILE3, sep=';')
 
-# Plot the data
-plt.plot(data['timestamp'], data['price'])
-plt.xlabel('Timestamp')
-plt.ylabel('Price')
+data2['timestamp'] += 1000000
+data3['timestamp'] += 2000000
 
-plt.xlim(0, 1010000) 
-plt.ticklabel_format(style='plain')
+data = pd.concat([data1, data2, data3], ignore_index=True)
 
-plt.title('Trades Over Time for ' + PRODUCT)
+data_vouchers = data[data['symbol'] == PRODUCT1] 
+data_extract = data[data['symbol'] == PRODUCT2]
+
+# Create figure
+fig, ax1 = plt.subplots()
+
+# Left axis (VEV_5400)
+ax1.plot(data_extract['timestamp'], data_extract['price'], color='red', label=PRODUCT1)
+ax1.set_ylabel(PRODUCT1)
+ax1.set_ylim(5190, 5320)
+
+# Right axis (VELVETFRUIT_EXTRACT)
+ax2 = ax1.twinx()
+ax2.plot(data_vouchers['timestamp'], data_vouchers['price'], label=PRODUCT2)
+ax2.set_ylabel(PRODUCT2)
+ax2.set_ylim(0, 39)
+
+# Shared x-axis
+ax1.set_xlabel('Timestamp')
+ax1.set_xlim(0, 3005000)
+ax1.ticklabel_format(style='plain')
+
+plt.title('Trades Over Time')
+
 plt.show()
